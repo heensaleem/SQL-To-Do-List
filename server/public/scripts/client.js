c383eeb16528850975f436453bb98c4258181186
heensaleem <heenakouser440@gmail.com>
2019-04-28 15:38:28 -0500

    POST AND GET are  working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,81 @@
 console.log('js');
 
+$( document ).ready( function(){
+  console.log( 'JQ' );
+  // Establish Click Listeners
+  setupClickListeners()
+  // load existing data on page load
+  getTasks();
 
+}); // end doc ready
 
+function setupClickListeners() {
+  $( '#createBtn' ).on( 'click', function(){
+    console.log( 'in createBtn on click' );
+    // get user input and put in an object
+  
+    let dataToSend = {
+      name: $('#nameIn').val(),
+      task: $('#taskIn').val(),
+      notes: $('#notesIn').val(),
+      taskStatus: $('#statusIn').val()
+    };
+    console.log(dataToSend)
+   
+    $('input').val('');
+    saveTasks( dataToSend );
+  });
+  $('#viewTasks').on('click', '.completeBtn', setToComplete);
+}
 
+function getTasks(){
+  console.log( 'in getdata' );
+  $.ajax ({
+    type: 'GET',
+    url: '/todolists'
+  }).then(function (response) {
+    console.log('response', response);
+    refreshTasks(response);
+  })
+} // end getTasks
 
+function saveTasks(newData){
+  console.log('in save tasks', newData);
+  $.ajax({
+    method: 'POST',
+    url: '/todolists',
+    data: newData
+  }).then(function(response) {
+      console.log(response);
+      getTasks();
+  }).catch( function( error ){
+    console.log( 'error getting To do list:', error );
+  })
+}
 
+function refreshTasks(lists){
+  console.log('in refreshtasks', lists);
+  let el = $('#viewTasks');
+  el.empty();
 
+  for(let list of lists){
+    console.log('lists', lists);
+    console.log('checking true/false:',list.taskStatus);
+    let taskstatusAppend =
+      (list.taskStatus) ? `<p class="completeBtn">completed</p>`
+        : `<button class="btn btn-primary completeBtn" data-id="${list.id}">uncompleted</button>`;
+    console.log('transfer append:', taskstatusAppend);
+    let append = `\
+      <tr>
+       <td>${list.name}</td>
+        <td>${list.task}</td>
+        <td>${list.notes}</td>
+        <td>${taskstatusAppend}</td>
+        <td><button class="btn btn-danger deleteBtn" data-id="${list.id}">X</button></td>
+      </tr>
+    `
+    console.log('appending:',append);
+    $('#viewTasks').append(append);
+  }
 
+}
--- server/routes/todolist.router.js
+++ server/routes/todolist.router.js
@@ -2,6 +2,36 @@ const express = require('express');
 const toDoRouter = express.Router();
 const pool = require('../modules/pool')
 
+toDoRouter.get('/', (req,res) => {
+  console.log('In GET route');
+  pool.query(`SELECT * FROM "todos"`)
+  .then((result) => {
+      console.log(result.rows);
+      res.send(result.rows);
+  }).catch((error)=> {
+      console.log('Error in GET', error);
+      res.sendStatus (500)
+  })
+})
+
+
+toDoRouter.post('/', (req, res) => {
+  const newTasks = req.body;
+  console.log(newTasks);
+  const queryText = `
+    INSERT INTO "todos"("name", "task", "notes", "taskStatus") 
+    VALUES ($1, $2, $3, $4);
+    `
+  pool.query(queryText, [newTasks.name,  newTasks.task, newTasks.notes, newTasks.taskStatus ])
+  .then((result) => {
+    console.log(result.rows);
+    res.sendStatus(201);
+  }).catch((error) => {
+    console.log(`error in POST /todolists`, error);
+    res.sendStatus(500);
+  })
+})
+
 
 
 
